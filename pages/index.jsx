68312e43662b6609c502916ecd60ed72ee0074df
Christian Harjuno <harjuno.tian@gmail.com>
2022-02-21 01:43:30 +0700

    Added new meta tags

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,6 +16,21 @@ export default function Home(props) {
         <meta name="googlebot" content="all"/>
         <meta name="robots" content="all"/>
         <meta name="google" content="nopagereadaloud" />
+        <meta name="title" content="Christian Harjuno"/>
+        <meta name="description" content="Personal website of Christian Harjuno"/>
+
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://harjuno-xyz.vercel.app//"/>
+        <meta property="og:title" content="Christian Harjuno"/>
+        <meta property="og:description" content="Personal website of Christian Harjuno"/>
+        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+        
+        <meta name="theme-color" content="#000343"/>
+        <meta property="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:url" content="https://harjuno-xyz.vercel.app//"/>
+        <meta property="twitter:title" content="Christian Harjuno"/>
+        <meta property="twitter:description" content="Personal website of Christian Harjuno"/>
+        <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
       </Head>
 
       <main className={styles.main}>
